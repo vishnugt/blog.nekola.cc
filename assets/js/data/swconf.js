0ef040829c7ebc63b4ef034236af4b4219f18823
vishnugt <vishnugt@users.noreply.github.com>
2025-01-12 21:30:36 +0000

    deploy: b8befc79efe302d27623001f656ae4e99a9d2a84

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1736717435',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://nekola.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
