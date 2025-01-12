9c3d1dc6aa47b3408e069dce9d4bf9bad77a11da
vishnugt <runner@fv-az1308-741.goeqwkruhmdevjehdg1fxxpbzh.bx.internal.cloudapp.net>
2025-01-12 22:14:48 +0000

    Updating the _site via Github Actions

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735922950',resources: [
+    cacheName: 'chirpy-1736720085',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
