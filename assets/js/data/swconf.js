58e0bdf9b7589dc7fe6d4f297fa353d00cb0b1d9
vishnugt <vishnugt@users.noreply.github.com>
2025-01-12 22:06:11 +0000

    deploy: e46934d03556a81ae3922546c40350c8cb04f048

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736717435',resources: [
+    cacheName: 'chirpy-1736719569',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
